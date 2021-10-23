32bb8124f79171624a8196af3c812d9fccd4a53b
Duy Nguyen <nguyenduy010798@gmail.com>
2021-10-24 01:57:07 +0300

    fix init script

--- backend/scripts/after_install.sh
+++ backend/scripts/after_install.sh
@@ -3,15 +3,15 @@
 cd /home/ubuntu/app
 sudo yarn install
 
-# port=$(aws ssm get-parameters --with-decryption \
-# --names "/production/server/redis/port" "/production/server/redis/endpoint" \
-# --region eu-north-1 | jq ".Parameters[0].Value")
+port=$(aws ssm get-parameters --with-decryption \
+--names "/production/server/redis/port" "/production/server/redis/endpoint" \
+--region eu-north-1 | jq ".Parameters[0].Value")
 
-# endpoint=$(aws ssm get-parameters --with-decryption \
-# --names "/production/server/redis/port" "/production/server/redis/endpoint" \
-# --region eu-north-1 | jq ".Parameters[1].Value")
+endpoint=$(aws ssm get-parameters --with-decryption \
+--names "/production/server/redis/port" "/production/server/redis/endpoint" \
+--region eu-north-1 | jq ".Parameters[1].Value")
 
-# sudo cat > .env <<EOF
-# PORT=${port}
-# ENDPOINT=${endpoint}
-# EOF
+sudo cat > .env <<EOF
+PORT=${port}
+ENDPOINT=${endpoint}
+EOF
