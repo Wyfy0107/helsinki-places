55d0dcbb4abad2d2d3f14bb3fed9b7218d64a58f
Duy Nguyen <nguyenduy010798@gmail.com>
2021-10-24 19:33:42 +0300

    add debug code

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,12 +5,19 @@ import app from './app'
 
 dotenv.config()
 
+const isTestEnv = process.env.NODE_ENV === 'test'
+
 console.log('port env', process.env.REDIS_PORT)
-const redisPort = Number(process.env.REDIS_PORT || 6379)
+let redisPort = Number(process.env.REDIS_PORT as string)
 console.log('redis port', redisPort)
-const redisHost = process.env.ENDPOINT || 'localhost'
+let redisHost = process.env.ENDPOINT as string
 console.log('host env', process.env.ENDPOINT)
 console.log('redis host', redisHost)
+
+if (isTestEnv) {
+  redisPort = 6379
+  redisHost = 'localhost'
+}
 
 export const client = new Redis({
   port: redisPort,
